4f154aead0303d73d03670155e2e06c083239015
tumulte <contact@tumulte.me>
2015-02-16 17:55:16 +0100

    adding kv

--- test.py
+++ test.py
@@ -10,24 +10,8 @@ from kivy.uix.label import Label
 
 class MyApp(App):
 
-    def build(self):
-        widget = Widget()
-        layout = BoxLayout(orientation='vertical')
-        button = Button(text='Hello world', font_size=14)
-        button2 = Button(text='Hello world2', font_size=14)
-        button3 = Button(text='Hello world3', font_size=14)
-        button4 = Button(text='Hello world4', font_size=14)
-        button.bind(on_press=self.doStuff)
-        button2.bind(on_press=self.doStuff)
-        button3.bind(on_press=self.doStuff)
-        button4.bind(on_press=self.doStuff)
-        layout.add_widget(button)
-        layout.add_widget(button2)
-        layout.add_widget(button3)
-        layout.add_widget(button4)
-        return layout
-    def doStuff(self, event):
-        print('bou')
+    def test(self):
+        print('bouh')
 
 
 if __name__ == '__main__':
